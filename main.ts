17f4d8bc303d729ce491abece3e030f0dacb20cd
maoxiaoke <maoxiaoke@outlook.com>
2024-06-11 14:27:19 +0800

    feat: init

--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	addIcon,
+} from "obsidian";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	fileLocation: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	fileLocation: "To-Do.md",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
@@ -16,74 +26,65 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon("list-todo", "Open Todo", (evt: MouseEvent) => {
+			this.openTodo();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "open-todo",
+			name: "Open Todo",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				this.openTodo();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new OpenTodoSettingTab(this.app, this));
 	}
 
-	onunload() {
+	async openTodo() {
+		const fileLocation = this.settings.fileLocation;
 
+		if (!fileLocation) {
+			new Notice("No file location specified");
+			return;
+		}
+
+		// If the file does not exist, create a new file
+		const file = this.app.vault.getAbstractFileByPath(fileLocation);
+
+		if (!file) {
+			await this.app.vault.create(fileLocation, "");
+		}
+
+		// 如果文件已经在 tab 中打开，直接 focus
+		const fileView = this.app.workspace
+			.getLeavesOfType("markdown")
+			.find((leaf) => {
+				return (
+					leaf.view instanceof MarkdownView &&
+					leaf.view.file &&
+					leaf.view.file.path === fileLocation
+				);
+			});
+
+		if (fileView) {
+			this.app.workspace.setActiveLeaf(fileView);
+			return;
+		}
+
+		this.app.workspace.openLinkText(fileLocation, fileLocation, true);
 	}
+
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,23 +92,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class OpenTodoSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -116,19 +101,56 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl("h3", { text: "Open Todo Settings" });
+
+		const mainDesc = containerEl.createEl("p");
+
+		mainDesc.appendText("For help, ");
+		mainDesc.appendChild(
+			createEl("a", {
+				text: "see documentation",
+				href: "https://github.com/maoxiaoke/image-resizer",
+			})
+		);
+		mainDesc.appendText(" or join ");
+		mainDesc.appendChild(
+			createEl("strong", {
+				text: "#nazha",
+			})
+		);
+		mainDesc.appendText(" in the ");
+		mainDesc.appendChild(
+			createEl("a", {
+				text: "Telegram Channel",
+				href: "https://t.me/+m3vKoZMPtoE5NDc1",
+			})
+		);
+		mainDesc.appendText(" community. See author on ");
+
+		mainDesc.appendChild(
+			createEl("a", {
+				text: "X",
+				href: "https://x.com/xiaokedada",
+			})
+		);
+
+		mainDesc.appendText(".");
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("File location")
+			.setDesc("Where should the file be opened?")
+			.addText((text) =>
+				text
+					.setPlaceholder("To-Do.md")
+					.setValue(this.plugin.settings.fileLocation)
+					.onChange(async (value) => {
+						this.plugin.settings.fileLocation = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
